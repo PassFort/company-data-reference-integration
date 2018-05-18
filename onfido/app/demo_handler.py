548b31cd1ce4f2bf24a833bea09b603df21e4ea3
Faris Keenan <faris.keenan@gmail.com>
2018-05-18 09:34:18 +0100

    demo handler

--- onfido/app/demo_handler.py
+++ onfido/app/demo_handler.py
@@ -2,10 +2,10 @@ from app.mock_data.mock_responses import mock_uk_response, mock_one_plus_one, mo
 
 
 def get_demo_response(name):
-    if 'fail' in " ".join(name.given_names).lower() or 'fail' in name.family_name.lower():
+    if 'fail' in " ".join(name['given_names']).lower() or 'fail' in name['family_name'].lower():
         # Return data which will trigger a fail on the stage
         return mock_fail
-    elif '1+1' in " ".join(name.given_names).lower() or '1+1' in name.family_name.lower():
+    elif '1+1' in " ".join(name['given_names']).lower() or '1+1' in name['family_name'].lower():
         # Return data which will trigger a 1+1 on the stage
         return mock_one_plus_one
 
