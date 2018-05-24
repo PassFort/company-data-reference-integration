ecb87e4f25cb5900f7ad52a92fcefbbe7d6c9f6f
Andreea Gherman <andreea@passfort.com>
2018-05-24 08:57:17 +0100

    Use swagger client from package

--- worldcheck/swagger_client/api/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from swagger_client.api.audit_api import AuditApi
-from swagger_client.api.case_api import CaseApi
-from swagger_client.api.groups_api import GroupsApi
-from swagger_client.api.reference_api import ReferenceApi
