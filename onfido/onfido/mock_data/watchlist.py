de02799bb57a11eed9d504cc13c05595103a0f2c
Rikin Patel <rikin@passfort.com>
2018-06-28 16:30:11 +0100

    Add demo data response to Onfido

---
+++ onfido/onfido/mock_data/watchlist.py
@@ -0,0 +1,273 @@
+consider = {'data':{
+  "id": "4469051b-b015-449d-8edb-a4c1ffaa14a5",
+  "created_at": "2018-06-28T12:43:36Z",
+  "status": "complete",
+  "redirect_uri": None,
+  "type": "express",
+  "result": "consider",
+  "sandbox": True,
+  "report_type_groups": [
+    "3766"
+  ],
+  "tags": [
+    
+  ],
+  "results_uri": "https://onfido.com/dashboard/information_requests/12873393",
+  "download_uri": "https://onfido.com/dashboard/pdf/information_requests/12873393",
+  "form_uri": None,
+  "href": "/v2/applicants/dc8c88eb-904f-49d4-9561-dfcaef2d2f98/checks/4469051b-b015-449d-8edb-a4c1ffaa14a5",
+  "reports": [
+    {
+      "created_at": "2018-06-28T12:43:36Z",
+      "href": "/v2/checks/4469051b-b015-449d-8edb-a4c1ffaa14a5/reports/c90653fe-f421-40cc-a577-4623c5a4fc5b",
+      "id": "c90653fe-f421-40cc-a577-4623c5a4fc5b",
+      "name": "watchlist",
+      "properties": {
+        "records": [
+          {
+            "full_name": "",
+            "date_of_birth": [
+              "1965-09-11",
+              "1965"
+            ],
+            "address": [
+              {
+                "locator_type": "BIRTH",
+                "town": "New York",
+                "state_province": "New York",
+                "country": "USA"
+              },
+              {
+                "address_line1": "Some Street",
+                "town": "London",
+                "state_province": "England",
+                "postal_code": "AA0A 0AA",
+                "country": "GBR"
+              },
+              {
+                "address_line1": "Another Street",
+                "town": "London",
+                "state_province": "England",
+                "postal_code": "BB0 0BB",
+                "country": "GBR"
+              }
+            ],
+            "event": [
+              {
+                "category": "Person Political",
+                "sub_category": "Associated with, Seen with",
+                "event_description": "On List [PEP Connect - UK]",
+                "event_date": "2018-01-01"
+              },
+              {
+                "category": "Bribery, Graft, Kickbacks, Political Corruption",
+                "sub_category": "Allege",
+                "event_description": "allegations of corruption",
+                "event_date": "2008-01-01"
+              }
+            ],
+            "attribute": [
+              {
+                "attribute_type": "Image URL",
+                "attribute_value": "https://some.website.com/photos/00000.jpg"
+              },
+              {
+                "attribute_type": "Sex",
+                "attribute_value": ""
+              },
+              {
+                "attribute_type": "PEP Type",
+                "attribute_value": "MUN:L1"
+              },
+              {
+                "attribute_type": "PEP Type",
+                "attribute_value": "POL:L2"
+              },
+              {
+                "attribute_type": "PEP Type",
+                "attribute_value": "CAB:L1"
+              },
+              {
+                "attribute_type": "PEP Type",
+                "attribute_value": "FAM"
+              },
+              {
+                "attribute_type": "PEP Type",
+                "attribute_value": "LEG:L1"
+              }
+            ],
+            "alias": [
+              {
+                "alias_type": "AKA",
+                "alias_name": "Test consider"
+              }
+            ],
+            "associate": [
+              {
+                "relationship_direction": "FROM",
+                "relationship_type": "GRANDFATHER",
+                "entity_name": "name 1"
+              },
+              {
+                "relationship_direction": "FROM",
+                "relationship_type": "STEP_FATHER",
+                "entity_name": "name 2"
+              },
+              {
+                "relationship_direction": "FROM",
+                "relationship_type": "FRIEND",
+                "entity_name": "name 3"
+              },
+              {
+                "relationship_direction": "FROM",
+                "relationship_type": "FRIEND",
+                "entity_name": "name 4"
+              },
+              {
+                "relationship_direction": "FROM",
+                "relationship_type": "FRIEND",
+                "entity_name": "name 5"
+              },
+              {
+                "relationship_direction": "FROM",
+                "relationship_type": "GRANDFATHER",
+                "entity_name": "name 6"
+              },
+              {
+                "relationship_direction": "FROM",
+                "relationship_type": "GRANDFATHER",
+                "entity_name": "name 7"
+              },
+              {
+                "relationship_direction": "FROM",
+                "relationship_type": "STEP_DAUGHTER",
+                "entity_name": "name 8"
+              },
+              {
+                "relationship_direction": "FROM",
+                "relationship_type": "WIFE",
+                "entity_name": "name 9"
+              },
+              {
+                "relationship_direction": "FROM",
+                "relationship_type": "DAUGHTER",
+                "entity_name": "name 10"
+              },
+              {
+                "relationship_direction": "FROM",
+                "relationship_type": "FATHER_IN_LAW",
+                "entity_name": "name 11"
+              },
+              {
+                "relationship_direction": "FROM",
+                "relationship_type": "FATHER",
+                "entity_name": "name 12"
+              },
+              {
+                "relationship_direction": "FROM",
+                "relationship_type": "MOTHER",
+                "entity_name": "name 13"
+              },
+              {
+                "relationship_direction": "FROM",
+                "relationship_type": "DAUGHTER",
+                "entity_name": "name 14"
+              },
+              {
+                "relationship_direction": "FROM",
+                "relationship_type": "SON",
+                "entity_name": "name 15"
+              },
+              {
+                "relationship_direction": "FROM",
+                "relationship_type": "ASSOCIATE",
+                "entity_name": "name 16"
+              }
+            ],
+            "position": [
+              "Position 1 - Role, Country (12/31/2016 - )",
+              "Position 2 - Role, Country (01/-/2015 - )",
+              "Position 3 - Role, Country (01/-/2008 - 01/-/2016)"
+            ],
+            "source": [
+              {
+                "source_name": "A newspaper, 01/10/2018, Headline – some description",
+                "source_url": "https://a.newspapper.com/news/0000000/article/",
+                "source_headline": "This document provides the position of "
+              },
+              {
+                "source_name": "Gov website",
+                "source_url": "https://a.gov.website/government/people/Test-consider",
+                "source_headline": "This document provides the position of "
+              },
+              {
+                "source_name": "A Radio - Headline, Profile - ",
+                "source_url": "http://a.radio.website/Test-consider-000000",
+                "source_headline": "This document provides the date of birth of "
+              }
+            ]
+          },
+          {
+            "full_name": "Test consider",
+            "date_of_birth": [
+              "1965"
+            ],
+            "address": [
+              {
+                "country": "GBR"
+              }
+            ],
+            "event": [
+              {
+                "category": "Person Political",
+                "sub_category": "Government Official",
+                "event_description": "On List [World Leaders]",
+                "event_date": "2016-01-01",
+                "source": {
+                  "source_name": "World Leaders",
+                  "source_url": "https://a.website.gov/library/publications/world-leaders/index.html",
+                  "source_date": "2017-01-01"
+                }
+              }
+            ],
+            "attribute": [
+              {
+                "attribute_type": "Entity URL",
+                "attribute_value": "https://a.website.gov/library/publications/world-leaders/UK.html"
+              }
+            ],
+            "alias": None,
+            "associate": None,
+            "position": [
+              "Position"
+            ],
+            "source": [
+              {
+                "source_name": "World Leaders",
+                "source_url": "https://a.website.gov/library/publications/world-leaders/index.html",
+                "source_headline": "World Leaders"
+              }
+            ]
+          }
+        ]
+      },
+      "result": "consider",
+      "status": "complete",
+      "sub_result": None,
+      "variant": "kyc",
+      "breakdown": {
+        "adverse_media": {
+          "result": "consider"
+        },
+        "sanction": {
+          "result": "clear"
+        },
+        "politically_exposed_person": {
+          "result": "consider"
+        }
+      }
+    }
+  ],
+  "paused": False,
+  "version": "2.0"
+}}
