3d1f94ef0084de9fcf8f1b2e8f844b5eaedb37c7
Andreea Gherman <andreea@passfort.com>
2018-10-19 11:03:35 +0100

    Retrieve base events from Comply Advantage

---
+++ comply_advantage/app/api/internal_types.py
@@ -0,0 +1,72 @@
+from schematics import Model
+from schematics.types.compound import ModelType, ListType, DictType
+from schematics.types import StringType
+
+from .types import ReferMatchEvent
+
+
+class ComplyAdvantageMatchField(Model):
+    name = StringType()
+    tag = StringType(default=None)
+    value = StringType()
+
+    def is_dob(self):
+        return self.tag == "date_of_birth"
+
+    class Options:
+        serialize_when_none = False
+
+
+class ComplyAdvantageMatchTypeDetails(Model):
+    type = StringType(default=None)
+
+    def is_aka(self):
+        return self.type == 'aka'
+
+
+class ComplyAdvantageMatchData(Model):
+    id = StringType(required=True)
+    name = StringType(required=True)
+    ca_fields = ListType(ModelType(ComplyAdvantageMatchField), serialized_name="fields")
+
+    def to_events(self, extra_fields):
+        birth_dates = set(field.value for field in self.ca_fields if field.is_dob())
+
+        result = ReferMatchEvent().import_data({
+            "match_id": self.id,
+            "match_name": self.name,
+            "provider_name": "Comply Advantage",
+            "match_dates": list(birth_dates),
+            **extra_fields
+        })
+        return [result]
+
+
+class ComplyAdvantageMatch(Model):
+    doc = ModelType(ComplyAdvantageMatchData, required=True)
+    match_types_details = DictType(ModelType(ComplyAdvantageMatchTypeDetails), default={})
+
+    def to_events(self):
+        aliases = set(k for k, v in self.match_types_details.items() if v.is_aka())
+        return self.doc.to_events({
+            'aliases': aliases
+        })
+
+
+class ComplyAdvantageResponseData(Model):
+    hits = ListType(ModelType(ComplyAdvantageMatch))
+
+    def to_events(self):
+        events = []
+
+        for hit in self.hits:
+            events = events + hit.to_events()
+        return events
+
+
+class ComplyAdvantageResponseContent(Model):
+    data = ModelType(ComplyAdvantageResponseData, required=True)
+
+
+class ComplyAdvantageResponse(Model):
+    content = ModelType(ComplyAdvantageResponseContent, required=True)
--- comply_advantage/app/request_handler.py
+++ comply_advantage/app/request_handler.py
@@ -1,5 +1,6 @@
 import requests
 from typing import TYPE_CHECKING
+from .api.internal_types import ComplyAdvantageResponse
 
 if TYPE_CHECKING:
     from .api.types import ScreeningRequestData, ComplyAdvantageConfig, ComplyAdvantageCredentials
@@ -23,6 +24,13 @@ def comply_advantage_search_request(
 
     # TODO add retry logic
     response = requests.post(url, json=data.to_provider_format(config))
-
+    # TODO paginate -> total_hits, offset, limit
     # TODO parse response into events
-    return response.json()
+    raw_response = response.json()
+    response_model = ComplyAdvantageResponse().import_data(raw_response, apply_defaults=True)
+    response_model.validate()
+    return {
+        "raw": raw_response,
+        "errors": [],
+        "events": [e.as_validated_json() for e in response_model.content.data.to_events()]
+    }
