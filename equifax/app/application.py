9fc82f92e3cd4a45e6e2a38cbec1eaf7bfb5789b
Andreea Gherman <andreea@passfort.com>
2019-01-28 10:06:52 +0000

    Add Equifax base service

---
+++ equifax/app/application.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, jsonify
+from raven.contrib.flask import Sentry
+import app.json_logger
+
+app = Flask(__name__)
+
+sentry_url = os.environ.get('SENTRY_URL')
+if sentry_url:
+    sentry = Sentry(
+        app,
+        logging=True,
+        level=logging.ERROR, dsn=sentry_url
+    )
+
+
+@app.route('/health')
+def health():
+    return jsonify('success')
