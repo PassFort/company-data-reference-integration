b71530ac141efff277a199918e3f54b80137d63b
Andreea Gherman <andreea@passfort.com>
2019-02-06 18:14:16 +0000

    Generate ekyc response from Equifax

--- equifax/app/test_request_handler.py
+++ equifax/app/test_request_handler.py
@@ -73,52 +73,139 @@ class TestErrorProcessing(unittest.TestCase):
     def test_expose_bad_configuration(self):
         actual = process_equifax_response(bad_security_code)
         self.assertEqual(
-            actual,
-            {
-                'errors': [
-                    {
-                        "code": ErrorCode.MISCONFIGURATION_ERROR.value,
-                        "message": "Invalid member number and/or security code",
-                        "source": "PROVIDER"
-                    }
-                ]
-            })
+            actual['errors'],
+            [
+                {
+                    "code": ErrorCode.MISCONFIGURATION_ERROR.value,
+                    "message": "Invalid member number and/or security code",
+                    "source": "PROVIDER"
+                }
+            ]
+        )
 
     def test_process_additional_info(self):
         actual = process_equifax_response(with_additional_info)
         self.assertEqual(
-            actual,
-            {
-                'errors': [
-                    {
-                        "code": ErrorCode.PROVIDER_UNKNOWN_ERROR.value,
-                        "message": "Invalid xml formatted input - missing xml tag (CustomerInfo)",
-                        "source": "PROVIDER"
-                    }
-                ]
-            }
+            actual['errors'],
+            [
+                {
+                    "code": ErrorCode.PROVIDER_UNKNOWN_ERROR.value,
+                    "message": "Invalid xml formatted input - missing xml tag (CustomerInfo)",
+                    "source": "PROVIDER"
+                }
+            ]
         )
 
     def test_process_multiple_errors(self):
         actual = process_equifax_response(bad_province_and_city)
         self.assertEqual(
-            actual,
-            {
-                'errors': [
-                    {
-                        "code": ErrorCode.PROVIDER_UNKNOWN_ERROR.value,
-                        "message": "Invalid city name",
-                        "source": "PROVIDER"
-                    },
-                    {
-                        "code": ErrorCode.PROVIDER_UNKNOWN_ERROR.value,
-                        "message": "Invalid province code",
-                        "source": "PROVIDER"
-                    }
-                ]
-            }
+            actual['errors'],
+            [
+                {
+                    "code": ErrorCode.PROVIDER_UNKNOWN_ERROR.value,
+                    "message": "Invalid city name",
+                    "source": "PROVIDER"
+                },
+                {
+                    "code": ErrorCode.PROVIDER_UNKNOWN_ERROR.value,
+                    "message": "Invalid province code",
+                    "source": "PROVIDER"
+                }
+            ]
         )
 
-    def test_process_successful_response(self):
+    def test_process_successful_response_signon(self):
         actual = process_equifax_response(success_response)
-        self.assertEqual(actual, {'errors': []})
+        self.assertEqual(actual['errors'], [])
+
+
+class TestResultProcessing(unittest.TestCase):
+
+    def get_and_assert_active_rule(self, rules, expected_active_index):
+        for index, rule in enumerate(rules):
+            if index == expected_active_index:
+                self.assertTrue(rule['is_active'])
+            else:
+                self.assertFalse(rule['is_active'])
+        return rules[expected_active_index]
+
+    def test_process_successful_response_2_plus_2(self):
+        with open('mock_data/2_plus_2.xml', 'rb') as f:
+            test_input = f.read()
+        actual = process_equifax_response(test_input)
+        self.assertEqual(actual['errors'], [])
+
+        matches = actual['output_data']['electronic_id_check']['matches']
+        self.assertEqual(len(matches), 2)
+
+        self.assertEqual(
+            sorted(matches[0]['matched_fields']),
+            ['ADDRESS', 'DOB', 'FORENAME', 'SURNAME']
+        )
+
+        rules = actual['output_data']['electronic_id_check']['rules']
+
+        active_rule = self.get_and_assert_active_rule(rules, 0)
+        self.assertEqual(active_rule['result'], '2+2')
+        self.assertEqual(len(active_rule['satisfied_by']), 2)
+
+    def test_process_1_plus_1_address(self):
+        with open('mock_data/1_plus_1_address.xml', 'rb') as f:
+                test_input = f.read()
+        actual = process_equifax_response(test_input)
+        self.assertEqual(actual['errors'], [])
+
+        matches = actual['output_data']['electronic_id_check']['matches']
+        self.assertEqual(len(matches), 1)
+
+        self.assertEqual(
+            sorted(matches[0]['matched_fields']),
+            ['ADDRESS', 'FORENAME', 'SURNAME']
+        )
+
+        rules = actual['output_data']['electronic_id_check']['rules']
+
+        active_rule = self.get_and_assert_active_rule(rules, 1)
+        self.assertEqual(active_rule['result'], '1+1')
+        self.assertEqual(len(active_rule['satisfied_by']), 1)
+        self.assertEqual(
+            sorted(active_rule['satisfied_by'][0]['matched_fields']),
+            ['ADDRESS', 'FORENAME', 'SURNAME'])
+
+    def test_process_1_plus_1_dob(self):
+        with open('mock_data/1_plus_1_dob.xml', 'rb') as f:
+            test_input = f.read()
+        actual = process_equifax_response(test_input)
+        self.assertEqual(actual['errors'], [])
+
+        matches = actual['output_data']['electronic_id_check']['matches']
+        self.assertEqual(len(matches), 1)
+
+        self.assertEqual(
+            sorted(matches[0]['matched_fields']),
+            ['ADDRESS', 'DOB', 'FORENAME', 'SURNAME']
+        )
+
+        rules = actual['output_data']['electronic_id_check']['rules']
+
+        active_rule = self.get_and_assert_active_rule(rules, 2)
+        self.assertEqual(active_rule['result'], '1+1')
+        self.assertEqual(len(active_rule['satisfied_by']), 1)
+        self.assertEqual(
+            sorted(active_rule['satisfied_by'][0]['matched_fields']),
+            ['DOB', 'FORENAME', 'SURNAME'])
+
+    def test_process_fail_no_trade(self):
+        with open('mock_data/fail_no_trade.xml', 'rb') as f:
+            test_input = f.read()
+        actual = process_equifax_response(test_input)
+        self.assertEqual(actual['errors'], [])
+
+        matches = actual['output_data']['electronic_id_check']['matches']
+        self.assertEqual(len(matches), 0)
+
+        rules = actual['output_data']['electronic_id_check']['rules']
+
+        active_rule = self.get_and_assert_active_rule(rules, 3)
+        self.assertEqual(active_rule['result'], 'Fail')
+        self.assertEqual(len(active_rule['satisfied_by']), 0)
