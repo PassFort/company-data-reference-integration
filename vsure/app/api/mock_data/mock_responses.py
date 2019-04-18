47abd8289c28e2749b3fcdf100bcefdd7b48069c
Charlotte Seabrok <charlotte.seabrook@passfort.com>
2019-04-18 15:46:46 +0100

    Added new tests

--- vsure/app/api/mock_data/mock_responses.py
+++ vsure/app/api/mock_data/mock_responses.py
@@ -1,17 +1,85 @@
-mock_successful_response = {
+unidentified_person_response = {
+    'errors': [{
+        'code': 303,
+        'info': {'raw': 'Error: VEVO cannot identify the person'},
+        'message': 'Error: VEVO cannot identify the person',
+        'source': 'PROVIDER'
+    }]
+}
+
+no_visa_response = {
+    "failure_reason": "Error: Does not hold a valid visa",
     "visas": [{
-        "grant_date": "14 Mar 2014",
-        "expiry_date": "N/A",
+        "details": [{
+            "name": "Visa Type",
+            "value": '0'
+        }],
+        'dob': '2000-12-12',
+        'entitlement': 'WORK',
+        'expiry_date': None,
+        'full_name': 'John Novisa',
+        'grant_date': None,
+        'name': None,
+        'document_checked': {
+            'document_type': 'PASSPORT',
+            'country_code': 'FIN',
+            'number': '37373737',
+        }
+    }]
+}
+
+login_error_response = {
+    'errors': [{
+        'code': 303,
+        'info': {'raw': 'Error: VEVO login error'},
+        'message': 'Error: VEVO login error',
+        'source': 'PROVIDER'
+    }]
+}
+
+successful_work_response = {
+    "failure_reason": "",
+    "visas": [{
+        "details": [
+            {'name': 'Work Entitlement Description', 'value':'The visa holder has unlimited right to work in Australia.'},
+            {'name': 'Visa Applicant', 'value':'Primary'},
+            {'name': 'Visa Class','value':'SI'},
+            {'name': 'Visa Type','value':'820'},
+            {'name': 'Visa Type Details','value':'For partners of Australian citizens and permanent residents'}
+        ],
+        'dob': '2000-01-01',
+        'document_checked': {
+            'country_code': 'IND',
+            'document_type': 'PASSPORT',
+            'number': '22222222',
+        },
+        'entitlement': 'WORK',
+        'expiry_date': None,
+        'full_name': 'John Smith',
+        'grant_date': '2014-03-14',
+        'name': 'Partner'
+    }]
+}
+
+successful_study_response = {
+    "visas": [{
+        'full_name': 'John Smith',
+        'dob': '1999-01-01',
+        "grant_date": "2014-03-14",
+        "expiry_date": None,
         "name": "Partner",
-        "entitlement": "WORK",
-        "details": {
-            "Work Entitlement Description": "The visa holder has unlimited right to work in Australia.",
-            "Visa Applicant": "Primary",
-            "Visa Class": "SI",
-            "Visa Type": "820",
-            "Visa Type Details": "For partners of Australian citizens and permanent residents",
-            "Visa Conditions": "",
-            "Conditions": ""
+        "entitlement": "STUDY",
+        "details": [
+            {"name": "Work Entitlement Description", "value": "The visa holder has unlimited right to work in Australia."},
+            {"name": "Visa Applicant", "value": "Primary"},
+            {"name": "Visa Class", "value": "SI"},
+            {"name": "Visa Type", "value": "820"},
+            {"name": "Visa Type Details", "value": "For partners of Australian citizens and permanent residents"},
+        ],
+        'document_checked': {
+            'document_type': 'PASSPORT',
+            'country_code': 'FIN',
+            'number': '37373737',
         }
     }],
     "failure_reason": ""
--- vsure/e2e/e2e.py
+++ vsure/e2e/e2e.py
@@ -1,23 +1,35 @@
 import requests
 import unittest
 import json
-from app.api.mock_data.mock_requests import mock_successful_request, mock_unidentified_person_request
-from app.api.mock_data.mock_responses import mock_successful_response
+from app.api.mock_data.mock_requests import successful_work_request, successful_study_request, \
+    unidentified_person_request, no_visa_request, login_error_request
+from app.api.mock_data.mock_responses import unidentified_person_response, no_visa_response, login_error_response, \
+     successful_work_response
 
 API_URL = 'http://localhost:8001'
+
 
 class Test(unittest.TestCase):
 
     def test_successful_response(self):
-        response = requests.post(f'{API_URL}/visa-check', json=mock_successful_request)
+        response = requests.post(f'{API_URL}/visa-check', json=successful_work_request)
 
         response_json = response.json()
 
-        self.assertEqual(mock_successful_response, response_json['output_data'])
+        self.assertEqual(successful_work_response, response_json['output_data'])
 
     def test_unidentified_person(self):
-    	response = requests.post(f'{API_URL}/visa-check', json=mock_unidentified_person_request)
+        response = requests.post(f'{API_URL}/visa-check', json=unidentified_person_request)
 
-    	response_json = response.json()
-    	print(response_json)
-    	self.assertEqual('', response_json)
+        response_json = response.json()
+
+        self.assertEqual(303, response_json['errors'][0]['code'])
+        self.assertEqual('Could not complete visa check - The Department has not been able to identify the person. Please check that the details you entered in are correct.', response_json['errors'][0]['message'])
+        self.assertEqual('PROVIDER', response_json['errors'][0]['source'])
+
+    def test_no_visa(self):
+        response = requests.post(f'{API_URL}/visa-check', json=no_visa_request)
+
+        response_json = response.json()
+
+        self.assertEqual(no_visa_response, response_json['output_data'])
