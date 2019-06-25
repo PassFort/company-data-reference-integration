6cb0250bfddf477a39151414ffbf6651d2a21ae1
Faris Keenan <faris.keenan@gmail.com>
2019-06-25 14:04:00 +0100

    yet another company type

--- duedil/app/metadata.py
+++ duedil/app/metadata.py
@@ -29,6 +29,11 @@ STRUCTURED_COMPANY_TYPE_MAP = {
         'is_limited': tagged(True),
         'ownership_type': tagged(OwnershipType.COMPANY)
     }),
+    'Limited Partnerships': StructuredCompanyType({
+        'is_public': tagged(False),
+        'is_limited': tagged(True),
+        'ownership_type': tagged(OwnershipType.COMPANY)
+    }),
     'Financial Product Companies – Licensed Resident Agent': StructuredCompanyType({
         'is_public': tagged(False),
         'is_limited': tagged(True),
