21fd9b4e780b8f79ddbe5f44e077885f7504226a
Henry Irish <me@henryirish.com>
2019-09-18 22:55:47 +0100

    Fix address formatter

--- uk_charities_commission/app/formatters.py
+++ uk_charities_commission/app/formatters.py
@@ -54,12 +54,17 @@ def format_address(address):
         freeform_address += f', {address.Postcode.strip()}'
 
     try:
-        return requests.post(f'http://{ADDRESS_PARSER}/parser', json={'query': freeform_address}).json()
+        formatted_address = requests.post(f'http://{ADDRESS_PARSER}/parser', json={'query': freeform_address}).json()
     except requests.exceptions.RequestException as e:
         print(e)
-        return {
+        formatted_address = {
             "original_freeform_address": freeform_address,
         }
+
+    return {
+        'type': 'registered_address',
+        'address': address,
+    }
 
 
 def format_trustee(trustee):
