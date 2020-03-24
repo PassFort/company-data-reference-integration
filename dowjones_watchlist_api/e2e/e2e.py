808aceeaafb94c6e4ee05ef89dca93e7c75af36e
Jack Cargill <jack@passfort.com>
2020-03-24 19:59:45 +0000

    Implement name matching for running demo checks
    Return mock data for checks where the input data contains one of the
    keywords:
    - PEP
    - SANCTION
    - MEDIA
    or where the fullname in input data matches one of:
    - 'Bashar Assad'
    - 'Hugo Chavez'
    - 'Robert Mugabe'
    - 'David Cameron'
    
    All comparisons are done case-insensitive.

--- dowjones_watchlist_api/e2e/e2e.py
+++ dowjones_watchlist_api/e2e/e2e.py
@@ -5,7 +5,6 @@ API_URL = 'http://localhost:8001'
 
 MOCK_PEIDS = ['873466', '2912358', '11847281', '4506663', '358285', '2984134']
 
-
 class TestApiValidation(unittest.TestCase):
 
     def test_health_check_passes(self):
@@ -38,8 +37,8 @@ class TestApiValidation(unittest.TestCase):
                     'entity_type': 'INDIVIDUAL',
                     'personal_details': {
                         'name': {
-                            'given_names': ['A_FIRST_NAME', 'A_MIDDLE_NAME'],
-                            'family_name': 'A_SECOND_NAME'
+                            'given_names': ['David', 'PEP'],
+                            'family_name': 'Cameron'
                         },
                     }
                 },
@@ -52,3 +51,122 @@ class TestApiValidation(unittest.TestCase):
 
         self.assertEqual(len(body['errors']), 0)
         self.assertEqual(len(body['events']), len(MOCK_PEIDS))
+
+
+class TestDemoChecks(unittest.TestCase):
+    def test_pep_demo(self):
+        response = requests.post(
+            f'{API_URL}/screening_request',
+            json={
+                'config': {
+                    'ignore_deceased': False,
+                    'include_adverse_media': True,
+                    'include_adsr': True,
+                    'include_associates': True,
+                    'include_oel': True,
+                    'include_ool': True,
+                    'search_type': 'BROAD',
+                    'strict_dob_search': True,
+                },
+                'credentials': {
+                    'namespace': 'A_NAMESPACE',
+                    'username': 'A_USERNAME',
+                    'password': 'A_PASSWORD',
+                    'url': 'A_URL',
+                },
+                'input_data': {
+                    'entity_type': 'INDIVIDUAL',
+                    'personal_details': {
+                        'name': {
+                            'given_names': ['David', 'PEP'],
+                            'family_name': 'Cameron'
+                        },
+                    }
+                },
+                'is_demo': True,
+            }
+        )
+        self.assertEqual(response.status_code, 200)
+
+        body = response.json()
+
+        self.assertEqual(len(body['errors']), 0)
+        self.assertTrue(any(event['event_type'] == 'PEP_FLAG' for event in body['events']))
+
+    def test_sanctions_demo(self):
+        response = requests.post(
+            f'{API_URL}/screening_request',
+            json={
+                'config': {
+                    'ignore_deceased': False,
+                    'include_adverse_media': True,
+                    'include_adsr': True,
+                    'include_associates': True,
+                    'include_oel': True,
+                    'include_ool': True,
+                    'search_type': 'BROAD',
+                    'strict_dob_search': True,
+                },
+                'credentials': {
+                    'namespace': 'A_NAMESPACE',
+                    'username': 'A_USERNAME',
+                    'password': 'A_PASSWORD',
+                    'url': 'A_URL',
+                },
+                'input_data': {
+                    'entity_type': 'INDIVIDUAL',
+                    'personal_details': {
+                        'name': {
+                            'given_names': ['Bashar', 'sanction'],
+                            'family_name': 'Assad'
+                        },
+                    }
+                },
+                'is_demo': True,
+            }
+        )
+        self.assertEqual(response.status_code, 200)
+
+        body = response.json()
+
+        self.assertEqual(len(body['errors']), 0)
+        self.assertTrue(any(event['event_type'] == 'SANCTION_FLAG' for event in body['events']))
+
+    def test_media_demo(self):
+        response = requests.post(
+            f'{API_URL}/screening_request',
+            json={
+                'config': {
+                    'ignore_deceased': False,
+                    'include_adverse_media': True,
+                    'include_adsr': True,
+                    'include_associates': True,
+                    'include_oel': True,
+                    'include_ool': True,
+                    'search_type': 'BROAD',
+                    'strict_dob_search': True,
+                },
+                'credentials': {
+                    'namespace': 'A_NAMESPACE',
+                    'username': 'A_USERNAME',
+                    'password': 'A_PASSWORD',
+                    'url': 'A_URL',
+                },
+                'input_data': {
+                    'entity_type': 'INDIVIDUAL',
+                    'personal_details': {
+                        'name': {
+                            'given_names': ['Hugo', 'media'],
+                            'family_name': 'Chavez'
+                        },
+                    }
+                },
+                'is_demo': True,
+            }
+        )
+        self.assertEqual(response.status_code, 200)
+
+        body = response.json()
+
+        self.assertEqual(len(body['errors']), 0)
+        self.assertTrue(any(event['event_type'] == 'ADVERSE_MEDIA_FLAG' for event in body['events']))
