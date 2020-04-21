d394998f88130fe98dc57d0214cc4f59efbc60b2
Andreea Gherman <andreea@passfort.com>
2020-04-21 16:23:01 +0100

    Add demo data for all countries
    * Improvements to the UK demo data flow
    * Add data for global reports (currency and country of incorporation are dynamic)

--- creditsafe/app/demo_handler.py
+++ creditsafe/app/demo_handler.py
@@ -1,8 +1,23 @@
 from .api.types import SearchInput, ReportInput
 from .api.internal_types import CreditSafeCompanyReport
 from .file_utils import get_response_from_file
+from demo_data.global_report_generator import get_report
+
 
 class DemoHandler:
+
+    def get_name_to_return(self, demo_creditsafe_id, country):
+        if country == 'GBR':
+            return {
+                'pass': 'PASSFORT LIMITED',
+                'partial': 'PASSFORT PARTIAL LIMITED',
+                'partial_financial': 'PASSFORT PARTIAL FINANCIAL'
+            }.get(demo_creditsafe_id)
+        return {
+            'pass': 'Aerial Traders',
+            'partial': 'Aerial Traders PARTIAL',
+            'partial_financial': 'Aerial Traders PARTIAL FINANCIAL'
+        }.get(demo_creditsafe_id)
 
     def search(self, input_data: 'SearchInput'):
         search_query = (input_data.query or input_data.name or '').lower()
@@ -10,27 +25,29 @@ class DemoHandler:
             return []
 
         creditsafe_id = 'pass'
-        name = 'PASSFORT LIMITED'
+
         if 'partial' in search_query:
             creditsafe_id = 'partial'
-            name = 'PASSFORT PARTIAL LIMITED'
 
         if 'partial_financial' in search_query:
             creditsafe_id = 'partial_financial'
-            name = 'PASSFORT PARTIAL FINANCIAL'
 
         result = {
-            'name': name,
-            'number': '09565115',
+            'name': self.get_name_to_return(creditsafe_id, input_data.country),
+            'number': '09565115' if input_data.country == 'GBR' else '5560642554',
             'creditsafe_id': creditsafe_id,
-            'country_of_incorporation': 'GBR'
+            'country_of_incorporation': input_data.country
         }
 
         return [result]
 
     def get_report(self, input_data: 'ReportInput'):
-        passfort_report = get_response_from_file('passfort', folder='demo_data/reports')
-        report = CreditSafeCompanyReport.from_json(passfort_report['report'])
+        if input_data.country_of_incorporation == 'GBR':
+            raw_report = get_response_from_file('passfort', folder='demo_data/reports')
+        else:
+            raw_report = get_report(input_data.creditsafe_id, input_data.country_of_incorporation)
+
+        report = CreditSafeCompanyReport.from_json(raw_report['report'])
 
         formatted_report = report.as_passfort_format_41()
 
@@ -38,4 +55,6 @@ class DemoHandler:
             formatted_report['metadata']['number'] = '1111111' # just return a different number
         if input_data.creditsafe_id == 'partial_financial':
             formatted_report['financials'] = None
+        formatted_report['metadata']['name'] = self.get_name_to_return(
+            input_data.creditsafe_id, input_data.country_of_incorporation)
         return formatted_report
