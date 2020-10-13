07c565e45d415639cda95fcd18b4d97c4464f33c
Charlotte Seabrook <charlotte.seabrook@passfort.com>
2020-10-13 16:50:31 +0100

    Add end_date to bvd monitoring callback

--- bvd_v4/app/passfort/client.py
+++ bvd_v4/app/passfort/client.py
@@ -7,13 +7,14 @@ class Client:
     def __init__(self, callback_url):
         self.callback_url = callback_url
 
-    def send_events(self, portfolio_id, portfolio_name, events, raw):
+    def send_events(self, portfolio_id, portfolio_name, end_date, events, raw):
         response = requests.post(
             self.callback_url,
             json=EventsCallback(
                 {
                     "portfolio_id": portfolio_id,
                     "portfolio_name": portfolio_name,
+                    "end_date": end_date,
                     "events": events,
                     "raw": raw,
                 }
