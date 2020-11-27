efcc9500f0d5ad5bd0089b63f209e50ed350e040
Tiberiu Simion Voicu <ts.voicu@icloud.com>
2020-11-27 17:35:03 +0000

    add types + test

---
+++ g2_kyc_automation/app/test_conversion.py
@@ -0,0 +1,130 @@
+from unittest import TestCase
+from app.types import CompanyData, G2Report, G2CompassResult, StructuredAddress
+from schematics.exceptions import DataError
+
+
+g2_report_data = {
+    "pdf_url": "https://kyc-files.s3.amazonaws.com/scan_pdfs/httpwwwexamplecom_2020-02-11_22-40-58-UTC_seACGC.pdf",
+    "messages": {
+        "warn": [
+            "The following required pages could not be found: Payment Policy",
+            "The following required pages could not be found: Refund Policy",
+        ],
+        "ok": [
+            "Prohibited phrases were not found.",
+            "Alexa reported no adult content."
+        ],
+        "crit": [
+            "YellowPages.com requires a business address.",
+            "OpenCorporates corporate search not run. No business address was provided."
+        ],
+        "info": [
+            "This is a high traffic site (Alexa rank 26037)"
+        ]
+    },
+    "g2_compass_results": [{
+        "compass_score": "1",
+        "reason_codes": [
+            {
+                "code": "105200",
+                "description": "Whois record is private"
+            },
+            {
+                "code": "105470",
+                "description": "Monitored without incident for more than 3 years, last monitored between 4 and 12 months ago"
+            }
+        ],
+        "valid_url": "true",
+        "fraudfile_matches": [],
+        "search_term": {
+            "merchant_name": "Blade HQ",
+            "merchant_url": "http://www.bladehq.com"
+        },
+        "site_active": "null",
+        "request_id": "197cb011-ec13-4295-a6a2-d5a44b9cc087",
+        "status": "complete"
+    }],
+    "malware_detections": { "phishings": 0, "malwares": 0 }
+}
+
+input_data = {
+    "entity_type": "COMPANY",
+    "metadata": {
+        "name": "Passfort",
+        "contact_details": {
+            "url": "https://www.passfort.com"
+        },
+        "addresses": [{
+            "type": "registered_address",
+            "address": {
+                "type": "STRUCTURED",
+                "country": "GBR",
+                "locality": "London",
+                "state_province": "England"
+            }
+        }]
+    },
+    "customer_ref": "79E25148-BB64-4280-962C-738436151487",
+}
+
+class TestFromPassfortToG2(TestCase):
+    def test_address_validation(self):
+        address = {
+            "type": "STRUCTURED",
+            "country": "GBR",
+            "locality": "LONDON"
+        }
+
+        with self.assertRaises(DataError):
+            address = StructuredAddress().import_data(address)
+            address.validate()
+
+
+    def test_minimal_input(self):
+        data: CompanyData = CompanyData().import_data(input_data, apply_defaults=True)
+        data.validate()
+
+        expected = {
+            "company_name": "Passfort",
+            "url": "https://www.passfort.com",
+            "business_address": {
+                "country": "GBR",
+                "city": "London",
+                "state": "England"
+            },
+            "vendor_id": "79E25148-BB64-4280-962C-738436151487"
+        }
+
+        self.assertEqual(expected, data.as_request())
+
+
+class TestFromG2ToPassfort(TestCase):
+    def test_parses_report_data(self):
+        report: G2Report = G2Report().import_data(g2_report_data, apply_defaults=True)
+        report.validate()
+
+        expected = {
+            "g2_compass_score": {
+                "score": 1,
+                "reason_codes": [
+                    {"name": "105200", "value": "Whois record is private"},
+                    {"name": "105470", "value": "Monitored without incident for more than 3 years, last monitored between 4 and 12 months ago"},
+                ],
+                "request_id": "197cb011-ec13-4295-a6a2-d5a44b9cc087",
+            },
+            "pdf_url": "https://kyc-files.s3.amazonaws.com/scan_pdfs/httpwwwexamplecom_2020-02-11_22-40-58-UTC_seACGC.pdf",
+            "messages": [
+                { "level": "CRITICAL", "description": "YellowPages.com requires a business address." },
+                { "level": "CRITICAL", "description": "OpenCorporates corporate search not run. No business address was provided."},
+                { "level": "WARNING", "description": "The following required pages could not be found: Payment Policy" },
+                { "level": "WARNING", "description": "The following required pages could not be found: Refund Policy" },
+                { "level": "INFO", "description": "This is a high traffic site (Alexa rank 26037)" },
+                { "level": "OK", "description": "Prohibited phrases were not found." },
+                { "level": "OK", "description": "Alexa reported no adult content." },
+            ],
+            "valid_url": True,
+            "is_active": None,
+        }
+
+        self.assertEqual(expected, report.to_passfort())
+
---
+++ g2_kyc_automation/app/types.py
@@ -0,0 +1,271 @@
+from enum import Enum, unique
+from schematics.common import NOT_NONE
+from schematics.exceptions import ValidationError
+from schematics.types.base import BooleanType, StringType, IntType
+from schematics.types.compound import ModelType, ListType
+from schematics import Model
+from datetime import datetime
+from flask import Response
+
+
+class MaybeBooleanType(BooleanType):
+    def to_native(self, value, context=None):
+        if isinstance(value, str):
+            if value in self.TRUE_VALUES:
+                value = True
+            elif value in self.FALSE_VALUES:
+                value = False
+
+        elif isinstance(value, int) and value in [0, 1]:
+            value = bool(value)
+
+        if not isinstance(value, bool):
+            # BooleanType impl would raise an error here
+            # we just set the value to None
+            # handles provider sending "null"
+            value = None
+
+        return value
+
+
+class Credentials(Model):
+    client_id = StringType(required=True)
+    client_secret = StringType(required=True)
+
+
+class Config(Model):
+    use_sandbox = BooleanType(default=False)
+    warn_zero_compass_score = BooleanType(default=False)
+    compass_score_threshold = IntType(default=500)
+
+
+
+def clean_dict(dict):
+    return {k: v for k, v in dict.items() if v is not None}
+
+
+class StructuredAddress(Model):
+    type = StringType(required=True)
+    country = StringType(required=True)
+    locality = StringType(default=None)
+    state_province = StringType(default=None)
+    route = StringType(default=None)
+    street_number = StringType(default=None)
+    postal_code = StringType(default=None)
+
+    class Options:
+        export_level = NOT_NONE
+
+    def validate_postal_code(self, data, value):
+        if not (data.get('postal_code') or (data.get('locality') and data.get('state_province'))):
+            raise ValidationError('Requires postal_code or state_province and locality!')
+        return value
+
+    def to_business_address(self):
+        ser_data = self.to_primitive()
+
+        return clean_dict({
+            'country': ser_data['country'],
+            'city': ser_data.get('locality'),
+            'postcode': ser_data.get('postal_code'),
+            'state': ser_data.get('state_province'),
+            'street': ser_data.get('route'),
+            'street_2': ser_data.get('street_number'),
+        })
+
+
+class CompanyAddress(Model):
+    address = ModelType(StructuredAddress, required=True)
+    type = StringType(required=True)
+
+
+class ContactDetails(Model):
+    url = StringType(required=True)
+
+
+class CompanyMetadata(Model):
+    name = StringType(required=True)
+    contact_details: ContactDetails = ModelType(ContactDetails, required=True)
+    addresses = ListType(ModelType(CompanyAddress), min_size=1, required=True)
+
+    @property
+    def address(self):
+        return self.addresses[0].address
+
+    @property
+    def url(self):
+        return self.contact_details.url
+
+
+class CompanyData(Model):
+    entity_type: str = StringType(required=True)
+    metadata: CompanyMetadata = ModelType(CompanyMetadata, required=True)
+    customer_ref: str = StringType(default=None)
+
+    def as_request(self):
+        request = {
+            "company_name": self.metadata.name,
+            "url": self.metadata.url,
+            "business_address": self.metadata.address.to_business_address(),
+        }
+
+        if self.customer_ref is not None:
+            return {
+                **request,
+                "vendor_id": self.customer_ref,
+            }
+        else:
+            return request
+
+
+class ReasonCode(Model):
+    code: str = StringType(required=True)
+    description: str = StringType(required=True)
+
+
+class G2CompassResult(Model):
+    compass_score = IntType(required=True)
+    reason_codes = ListType(ModelType(ReasonCode), default=[])
+    site_active = MaybeBooleanType(default=None)
+    valid_url = MaybeBooleanType(default=None)
+    request_id = StringType(required=True)
+
+    class Options:
+        export_level = NOT_NONE
+
+
+class Messages(Model):
+    crit = ListType(StringType(), default=[])
+    warn = ListType(StringType(), default=[])
+    info = ListType(StringType(), default=[])
+    ok = ListType(StringType(), default=[])
+
+    def get_joined(self):
+        def build_messages(level, msgs):
+            return [{
+                'level': level,
+                'description': msg
+            } for msg in msgs]
+
+        critical = build_messages('CRITICAL', self.crit)
+        warn = build_messages('WARNING', self.warn)
+        info = build_messages('INFO', self.info)
+        ok = build_messages('OK', self.ok)
+
+        return critical + warn + info + ok
+
+
+class G2Report(Model):
+    g2_compass_results = ListType(ModelType(G2CompassResult))
+    pdf_url: str = StringType(required=True)
+    messages: Messages = ModelType(Messages, required=True, default={})
+
+    @property
+    def compass_result(self):
+        return self.g2_compass_results[0]
+
+    def to_passfort(self):
+        messages = self.messages.get_joined()
+        reason_codes = [{
+            "name": x.code,
+            "value": x.description
+        } for x in self.compass_result.reason_codes]
+
+        ser_compass_result = self.compass_result.to_primitive()
+
+        return {
+            "g2_compass_score": {
+                "score": ser_compass_result['compass_score'],
+                "reason_codes": reason_codes,
+                "request_id": ser_compass_result.get('request_id'),
+            },
+            "messages": messages,
+            "pdf_url": self.pdf_url,
+            "valid_url": ser_compass_result.get('valid_url'),
+            "is_active": ser_compass_result.get('site_active'),
+        }
+
+
+@unique
+class ErrorCode(Enum):
+    INVALID_INPUT_DATA = 201
+    PROVIDER_MISCONFIGURATION_ERROR = 205
+    PROVIDER_CONNECTION_ERROR = 302
+    PROVIDER_UNKNOWN_ERROR = 303
+    UNKNOWN_INTERNAL_ERROR = 401
+
+
+class G2Exception(Exception):
+    def __init__(self, response: Response):
+        self.response = response
+
+
+class Error(object):
+    @staticmethod
+    def bad_api_request(e):
+        return {
+            'code': ErrorCode.INVALID_INPUT_DATA.value,
+            'source': 'API',
+            'message': 'Bad API request',
+            'info': e.to_primitive()
+        }
+
+    @staticmethod
+    def provider_connection_error(e):
+        return {
+            'code': ErrorCode.PROVIDER_CONNECTION_ERROR.value,
+            'source': 'PROVIDER',
+            'message': 'Connection error when contacting G2',
+            'info': {
+                'raw': '{}'.format(e)
+            }
+        }
+
+    @staticmethod
+    def provider_misconfiguration_error(e):
+        return {
+            'code': ErrorCode.PROVIDER_MISCONFIGURATION_ERROR.value,
+            'source': 'PROVIDER',
+            'message': f"Provider Configuration Error: '{e}' while running the 'G2' service",
+            'info': {
+                "Provider": "G2",
+                "Timestamp": str(datetime.now())
+            }
+        }
+
+    @staticmethod
+    def provider_unknown_error(e):
+        return {
+            'code': ErrorCode.PROVIDER_UNKNOWN_ERROR.value,
+            'source': 'PROVIDER',
+            'message': e or 'There was an error calling G2',
+            'info': {
+                'Provider': 'G2',
+                'Timestamp': str(datetime.now())
+            }
+        }
+
+    @staticmethod
+    def from_exception(e):
+        return {
+            'code': ErrorCode.UNKNOWN_INTERNAL_ERROR.value,
+            'source': 'ENGINE',
+            'message': '{}'.format(e)
+        }
+
+
+class RequestBase(Model):
+    credentials = ModelType(Credentials, required=True)
+    config = ModelType(Config, required=True)
+
+
+class ScreeningRequest(RequestBase):
+    input_data = ModelType(CompanyData, required=True)
+
+
+class PollRequest(RequestBase):
+    case_id = StringType(required=True)
+
+
+class ReportRequest(RequestBase):
+    case_id = StringType(required=True)
